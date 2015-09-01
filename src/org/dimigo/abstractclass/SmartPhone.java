9bce74383758e08cf1ca9796233e311e4283478c
Like <Like@Like-PC>
2015-09-01 14:02:25 +0900

    abstractclass SmartPhone

---
+++ src/org/dimigo/abstractclass/SmartPhone.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ *  <pre>
+ * org.dimigo.inheritance
+ * |_SmartPhone
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 27. 
+ * </pre>
+ *
+ * @author : 윤병창
+ * @version : 1.0
+ */
+public abstract class SmartPhone {
+	private String model;
+	private String company;
+	private int price;
+	
+	public SmartPhone()	{
+		
+	}
+	
+	public SmartPhone(String model, String company, int price)	{
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void turnOn()	{
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff()	{
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public abstract void pay();
+	
+	public void useSpecialFunctiin(SmartPhone phone)	{
+		if(phone instanceof IPhone)	((IPhone)phone).useAirDrop();
+		else if(phone instanceof Galaxy)	((Galaxy)phone).useWirelessCharging();
+	}
+
+	public String toString() {
+		return "Model : " + model + ", Company : " + company + ", Price : " + String.format("%,d", price) + "￦";
+	}
+	
+	
+
+}
