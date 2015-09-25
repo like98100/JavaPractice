77725a18040785b57caaf512aa1851b195390fa5
Like <Like@Like-PC>
2015-09-25 13:10:23 +0900

    Movie

---
+++ src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ *  <pre>
+ * org.dimigo.exception
+ * |_MovieTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 9. 22. 
+ * </pre>
+ *
+ * @author : 윤병창
+ * @version : 1.0
+ */
+public class MovieTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try	{
+			Movie[] movies = {
+					new Movie("앤트맨", 12), new Movie("사도", 12), new Movie("베테랑", 15)
+			};
+			int age = 13;
+			for(Movie movie : movies)	{
+				buyTicket(movie, age);	//여기서 예외 던짐
+			}
+		}	catch(Exception e)		{
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+	public static void buyTicket(Movie movie, int age)	throws AgeCheckException{
+	if(movie.getLimitAge() > age)	throw new AgeCheckException(movie);
+		else	System.out.println(movie.getTitle() + " 영화 즐감하세요");
+	}
+
+}
