f0c4e3c28bbd7cf7d3cea0ce1ea67f07f32ec3e6
Like <Like@Like-PC>
2015-10-27 14:10:35 +0900

    Melon

--- src/org/dimigo/collection/MelonChart.java
+++ src/org/dimigo/collection/MelonChart.java
@@ -4,9 +4,9 @@
 package org.dimigo.collection;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+//import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 /**
  *  <pre>
